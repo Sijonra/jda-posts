96bea7ab29feb9e84a37b471cbb5e14f83021808
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2023-11-02 17:40:21 +0300

    feat[JDA-0302]: add users request to api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,12 @@
-export const postsApi = {
+export const appApi = {
   getPosts: () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  },
+
+  getUsers: () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((json) => console.log(json));
   },
