70fab6f11bccf9bb221966817617a86acaef486c
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2023-11-06 12:20:26 +0300

    feat[JDA-0401]: add sorting by author name

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -5,17 +5,19 @@ interface SelectProps {
   className?: string;
   name: string;
   options: Array<string>;
+  selectedAuthor: string;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const Select: FC<SelectProps> = ({ name, options, className }) => {
+const Select: FC<SelectProps> = ({ name, options, className, selectedAuthor, onChange }) => {
 
   const updatedOptions = ["All", ...options];
-
+  
   return (
     <div className={style["select-wrapper"] + " " + className}>
-      <select className={style["select"]} name={name} id="">
+      <select value={selectedAuthor} onChange={onChange} className={style["select"]} name={name} id="">
         {updatedOptions.map((item) => {
-          return <option key={item} value={item}>{item}</option>;
+          return <option  key={item} >{item}</option>;
         })}
       </select>
     </div>
--- src/pages/PostsPage/PostsPage.tsx
+++ src/pages/PostsPage/PostsPage.tsx
@@ -3,13 +3,23 @@ import Input from "../../components/Input/Input";
 import Select from "../../components/Select/Select";
 // import Checkbox from "../../components/Checkbox/Checkbox";
 import Post from "../../components/Post/Post";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {PostsDataContext, UsersDataContext} from "../../App";
 
 const PostsPage = () => {
 
   const posts = useContext(PostsDataContext);
   const users = useContext(UsersDataContext);
+  const [selectedAuthor, setSelectedAuthor] = useState('All');
+
+  const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) =>{
+    setSelectedAuthor(event.target.value)
+  }
+
+  const filteredPosts = selectedAuthor === "All" ? posts : posts.filter((post)=>{
+    const user = users.find((u) => u.id === post.userId);
+    return user && user.name === selectedAuthor;
+  })
   
   return (
     <section className={style["posts-page"]}>
@@ -28,11 +38,13 @@ const PostsPage = () => {
                 return user.name
               })
             }
+            selectedAuthor={selectedAuthor}
+            onChange={onSelectChange}
           />
           {/* <Checkbox label="пост завершен" isDisabled={false} isChecked={true} /> */}
         </div>
         <div className={style["cards"]}>
-        {posts.map((post) => {
+        {filteredPosts.map((post) => {
             const user = users.find((u) => u.id === post.userId);
             return user ? <Post key={post.id} user={user} post={post} /> : null;
           })}
